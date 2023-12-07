3e6562d72b3745696e6ec99aeed0fe77353f7824
Danya Kovinskiy <129722708+Danilo666228@users.noreply.github.com>
2023-12-07 11:36:13 +0300

    Add files via upload

---
+++ PaymentOrder/PaymentOrder/PaymentOrder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentOrder
+{
+    internal class PaymentOrder
+    {
+        private string Person;
+        private double Price;
+        private string BankPerson;
+        private int BIK;
+        private int INN;
+        private string BankRecipient;
+        private long CheckRecipient;
+        
+
+        public PaymentOrder(string person, double price, string bankperson,int bik,int inn,string bankrecipient, long checkrecipient)
+        {
+            this.Person = person;
+            this.Price = price;
+            this.BankPerson = bankperson;
+            this.BIK = bik;
+            this.INN = inn;
+            this.BankRecipient = bankrecipient;
+            this.CheckRecipient = checkrecipient;
+           
+                      
+        }
+       public string ShowInfo()
+        {
+            string str;
+            return str = $"Плательщик: {Person}\nCумма: {Price}\nБанк плательщика: {BankPerson}\nБик: {BIK}\nИНН: {INN}\nБанк получателя: {BankRecipient}\nСчёт получателя: {CheckRecipient}";
+        }
+        
+    }
+}
